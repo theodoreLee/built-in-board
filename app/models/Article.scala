3dd145a4d53ecc01536c92e83dd514141ff4ec62
mydream0922 <mydream0922@gmail.com>
2012-12-25 22:09:43 +0900

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	app/controllers/Articles.scala

--- app/models/Article.scala
+++ app/models/Article.scala
@@ -8,30 +8,30 @@ package models
  * To change this template use File | Settings | File Templates.
  */
 
-case class Article(id:Long,
-                   title:String,
-                   contents:String,
-                   name:String,
-                   password:String) {
+case class Article(id: Long,
+                   title: String,
+                   contents: String,
+                   name: String,
+                   password: String) {
 }
 
 object Article {
   var data = Set(
-    Article(1,"Hi", "test", "thlee","1234"),
-    Article(2,"메렁~", "믕믕믕", "thlee","1234"),
-    Article(3,"ㅋㅋㅋㅋㅋ", "뭥미~? ㅋ", "thlee","1234")
+    Article(1, "Hi", "test", "thlee", "1234"),
+    Article(2, "메렁~", "믕믕믕", "thlee", "1234"),
+    Article(3, "ㅋㅋㅋㅋㅋ", "뭥미~? ㅋ", "thlee", "1234")
   )
 
   private def getNextID = this.data.size + 1
 
-  def getList:List[Article] = data.toList.sortBy(_.id)
+  def getList: List[Article] = data.toList.sortBy(_.id)
 
-  def get(id:Long):Option[Article] = this.data.find((article) => article.id == id)
+  def get(id: Long): Option[Article] = this.data.find((article) => article.id == id)
 
-  def add(item:Article):Option[Article] = {
+  def add(item: Article): Option[Article] = {
     item.id match {
       case -1 =>
-        val newItem = apply(item.title,item.contents,item.name,item.password)
+        val newItem = apply(item.title, item.contents, item.name, item.password)
         this.data = this.data + newItem
         Some(newItem)
       case id if (this.data.find((article) => article.id == item.id).isEmpty) =>
@@ -42,7 +42,7 @@ object Article {
     }
   }
 
-  def apply(title:String,contents:String,name:String,password:String):Article = {
-    Article(getNextID,title,contents,name,password)
+  def apply(title: String, contents: String, name: String, password: String): Article = {
+    Article(getNextID, title, contents, name, password)
   }
 }
