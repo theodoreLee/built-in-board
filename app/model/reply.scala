9ae5b9c7b26a3eb9bec7b2bf5ed9ade622e0cb6a
mydream0922 <mydream0922@gmail.com>
2012-12-25 19:35:17 +0900

    Reply class

---
+++ app/model/reply.scala
@@ -0,0 +1,18 @@
+package model
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yoonjilee
+ * Date: 12. 12. 25.
+ * Time: 오후 7:24
+ * To change this template use File | Settings | File Templates.
+ */
+case class Reply(id:Long, ArticleId:Long, contents:String, name:String, password:String )
+object Reply {
+  var replies = Set(Reply(123,1,"contents","play1","play1"),
+    Reply(234,2,"contents","play2","play2"),
+    Reply(345,3,"contents","play3","play3"),
+    Reply(2,4,"contents","play4","play4"),
+    Reply(12,5,"contents","play5","play5"),
+    Reply(13,6,"contents","play6","play6"))
+}
