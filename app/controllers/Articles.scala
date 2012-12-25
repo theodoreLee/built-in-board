acaee6e06fe892fdda1bb91dfcc7ed9ad31bdcd9
TaeHwanLee <thlee@manaolabs.com>
2012-12-25 20:55:13 +0900

    Add: Articles

---
+++ app/controllers/Articles.scala
@@ -0,0 +1,23 @@
+package controllers
+
+import play.mvc.Controller
+import play.api.mvc._
+import play.api.mvc.Results._
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thlee
+ * Date: 12. 12. 25.
+ * Time: 오후 8:44
+ * To change this template use File | Settings | File Templates.
+ */
+object Articles extends Controller{
+  def getArticle(id:Long) = Action {
+    NotImplemented
+  }
+
+  def showList = Action {
+    NotImplemented
+  }
+
+}
